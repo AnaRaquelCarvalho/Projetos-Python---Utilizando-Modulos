d78d6e241704f38b22b50b1e3b21a8eeb17feb79
Ana R. Carvalho <137975286+AnaRaquelCarvalho@users.noreply.github.com>
2023-08-02 23:33:37 -0300

    Create RandomM.py
    Random Aleatórios e de número inteiro

---
+++ RandomM.py
@@ -0,0 +1,11 @@
+
+print('='*6, 'Valores aleatórios', '='*6)
+import random
+num = random.random()
+print(num)
+
+print('='*4,'Executando um random de número inteiro','='*4)
+import random
+n1 = random.randint(1,10)
+print(n1)
+print('='*36)
